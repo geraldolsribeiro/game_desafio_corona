8a53e78adf73f61fca638622d6a86e6e0c967182
Geraldo Ribeiro <geraldolsribeiro@gmail.com>
2020-07-09 22:32:27 -0300

    Horda de coronas

--- main.lua
+++ main.lua
@@ -7,15 +7,13 @@ player = {
   y = 100
 }
 
-corona = {
-  speed = -150,
-  img = nil,
-  x = 400,
-  y = 200
-}
+coronaImg = nil
+coronas = {}
+createEnemyTimerMax = 0.4
+createEnemyTimer = createEnemyTimerMax
 
 function love.load(arg)
-  corona.img = love.graphics.newImage('assets/corona.png')
+  coronaImg = love.graphics.newImage('assets/corona.png')
   player.img = love.graphics.newImage('assets/player.png')
 end
 
@@ -33,9 +31,31 @@ function love.update(dt)
   elseif love.keyboard.isDown('down','s') then
     player.y = player.y + (player.speed*dt)
   end
+
+  -- Time out enemy creation
+  createEnemyTimer = createEnemyTimer - (1 * dt)
+  if createEnemyTimer < 0 then
+    createEnemyTimer = createEnemyTimerMax
+
+    -- Create an enemy
+    randomNumber = math.random(10, love.graphics.getHeight() - 10)
+    newCorona = { y = randomNumber, x = love.graphics.getWidth() -10, img = coronaImg }
+    table.insert( coronas, newCorona )
+  end
+
+  -- update the positions of enemies
+  for i, corona in ipairs(coronas) do
+    corona.x = corona.x - (200 * dt)
+
+    if corona.x < -64 then -- remove enemies when they pass off the screen
+      table.remove(coronas, i)
+    end
+  end
 end
 
 function love.draw(dt)
-  love.graphics.draw(corona.img, corona.x, corona.y)
   love.graphics.draw(player.img, player.x, player.y)
+  for i, corona in ipairs(coronas) do
+    love.graphics.draw(corona.img, corona.x, corona.y)
+  end
 end

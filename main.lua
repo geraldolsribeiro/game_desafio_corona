a6e44ce45f6fa591f45753d46b0ad7a3cb010ed8
Geraldo Ribeiro <geraldolsribeiro@gmail.com>
2020-07-09 22:14:06 -0300

    Movimentação do player

--- main.lua
+++ main.lua
@@ -1,11 +1,17 @@
 debug = true
 
 player = {
-  img = nil
+  speed = 150,
+  img = nil,
+  x = 100,
+  y = 100
 }
 
 corona = {
-  img = nil
+  speed = -150,
+  img = nil,
+  x = 400,
+  y = 200
 }
 
 function love.load(arg)
@@ -14,10 +20,22 @@ function love.load(arg)
 end
 
 function love.update(dt)
+  if love.keyboard.isDown('escape') then
+    love.event.push('quit')
+  end
 
+  if love.keyboard.isDown('left','a') then
+    player.x = player.x - (player.speed*dt)
+  elseif love.keyboard.isDown('right','d') then
+    player.x = player.x + (player.speed*dt)
+  elseif love.keyboard.isDown('up','w') then
+    player.y = player.y - (player.speed*dt)
+  elseif love.keyboard.isDown('down','s') then
+    player.y = player.y + (player.speed*dt)
+  end
 end
 
 function love.draw(dt)
-  love.graphics.draw(corona.img, 300, 100)
-  love.graphics.draw(player.img, 100, 100)
+  love.graphics.draw(corona.img, corona.x, corona.y)
+  love.graphics.draw(player.img, player.x, player.y)
 end
